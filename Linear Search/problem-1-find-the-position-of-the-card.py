8dad0c114cdbc7b5ff01a866791b2be0ba7dc0e6
Aromal Nambiar <aromalndr@gmail.com>
2023-04-09 13:13:43 +0530

    import more print statement for clarify the code

--- Linear Search/problem-1-find-the-position-of-the-card.py
+++ Linear Search/problem-1-find-the-position-of-the-card.py
@@ -3,13 +3,21 @@ def card_game(cards, query):
     # Create a variable position with the value 0    
     position = 0
     
+    #print cards and query 
+    print("cards : " , cards)
+    print("query : ", query)
+    
     # Set up a loop for repetition
     while True:
+        
+        #print the position
+        print("position : ", position)
         
         # Check if element at the current position matche the query
         if cards[position] == query:
             # Answer found! 
-            print(position)
+            print("\nAnwser is ", position)
+            return position
         
          # If its not the position then increment the position
         position += 1
--- Linear Search/test-case.py
+++ Linear Search/test-case.py
@@ -1,3 +1,17 @@
+
+# test case
+
+# 1. The number query occurs somewhere in the middle of the list cards.
+# 2. query is the first element in cards.
+# 3. query is the last element in cards.
+# 4. The list cards contains just one element, which is query.
+# 5. The list cards does not contain number query.
+# 6. The list cards is empty.
+# 7. The list cards contains repeating numbers.
+# 8. The number query occurs at more than one position in cards.
+# (can you think of any more variations?)
+
+
 [{'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 7}, 'output': 3},
  {'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 1}, 'output': 6},
  {'input': {'cards': [4, 2, 1, -1], 'query': 4}, 'output': 0},
